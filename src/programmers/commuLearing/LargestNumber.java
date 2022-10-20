5431918f196d88fad3e81354ba723080adf49ac4
victoriaum <y.ilpyo95@gmail.com>
2022-10-20 17:49:21 +0900

    LargestNumber.java create

---
+++ src/programmers/commuLearing/LargestNumber.java
@@ -0,0 +1,22 @@
+package programmers.commuLearing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LargestNumber {
+    public static void main(String[] args){
+        /*
+            1번째 제출풀이(firstSolution):
+            2번째 제출풀이(secondSoloution):
+
+        */
+        System.out.println( firstSolution(11, new int[]{4,11}, 1) );
+    }
+
+
+    public static int firstSolution(int n, int[] stations, int w) {
+        int answer = 0;
+        return answer;
+    }
+}
