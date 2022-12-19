4ca5ba431086a24040ae259bd6a805ae72abf9b9
victoriaum <y.ilpyo95@gmail.com>
2022-12-19 15:02:02 +0900

    BestAlbum.java update

--- src/programmers/kit/Hash/BestAlbum.java
+++ src/programmers/kit/Hash/BestAlbum.java
@@ -13,49 +13,43 @@ public class BestAlbum {
         String[] genres = {"classic", "pop", "classic", "classic", "pop"};
         int[] plays = {500, 600, 150, 800, 2500};
 
-        System.out.println(solution(genres, plays));   // 정확성 실패(런타임 에러)
-        System.out.println(trial(genres, plays));   // 정확성 실패(런타임 에러)
+        System.out.println(solution(genres, plays));   // 성공, 차리리 변수를 더 쓰더라도 반복문을 줄이자..
+        System.out.println(trial(genres, plays));      // 정확성 실패(런타임 에러)
     }
 
-    private static List<Integer> solution(String[] genres, int[] plays) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> total = new HashMap<>();
+    // 다른 사람 풀이 참조
+    private static int[] solution(String[] genres, int[] plays) {
+        ArrayList<Integer> answer = new ArrayList<>();
 
-        Set<String> set = Arrays.stream(genres).collect(Collectors.toSet());
-        for(int i=0; i<genres.length; i++){
-            total.put(genres[i], total.getOrDefault(plays[i],0)+plays[i]);
-            if(i==set.size()){ break; }
-        }
-
-        List<String> total_genres = new ArrayList<>();
-        for(String genre : set){
-            if(total_genres.size()!=0 && total.get(total_genres.get(0)) < total.get(genre)){
-                total_genres.add(0, genre);
-            }else{
-                total_genres.add(genre);
+        HashMap<String, Integer> num = new HashMap<>(); // 장르별 총 개수
+        HashMap<String, HashMap<Integer, Integer>> music = new HashMap<>(); // 장르에 속하는 노래 및 재생횟수
+        for(int i = 0; i < plays.length; i++) {
+            if(!num.containsKey(genres[i])) {
+                HashMap<Integer, Integer> map = new HashMap<>();
+                map.put(i, plays[i]);
+                music.put(genres[i], map);
+                num.put(genres[i], plays[i]);
+            } else {
+                music.get(genres[i]).put(i, plays[i]);
+                num.put(genres[i], num.get(genres[i]) + plays[i]);
             }
         }
 
-        List<Integer> temp;
-        int cnt;
-        Map<Integer, Integer> tempMap = new HashMap<>();
+        List<String> keySet = new ArrayList(num.keySet());
+        Collections.sort(keySet, (s1, s2) -> num.get(s2) - (num.get(s1)));
 
-        for(int i=0; i<total_genres.size(); i++){
-            temp = new ArrayList<>();
-            cnt = 0;
-            for(int j=0; j<plays.length; j++){
-                if(total_genres.get(i).equals(genres[j])) {
-                    temp.add(plays[j]);
-                    cnt++;
-                    if (tempMap.get(plays[j]) == null) tempMap.put(plays[j], j);
-                }
-            }
-            Collections.sort(temp, Collections.reverseOrder());
-            answer.add(tempMap.get(temp.get(0)));
-            answer.add(tempMap.get(temp.get(1)));
+        for(String key : keySet) {
+            HashMap<Integer, Integer> map = music.get(key);
+            List<Integer> genre_key = new ArrayList(map.keySet());
+
+            Collections.sort(genre_key, (s1, s2) -> map.get(s2) - (map.get(s1)));
+
+            answer.add(genre_key.get(0));
+            if(genre_key.size() > 1)
+                answer.add(genre_key.get(1));
         }
 
-        return answer;
+        return answer.stream().mapToInt(i -> i).toArray();
     }
 
     private static List<Integer> trial(String[] genres, int[] plays) {
