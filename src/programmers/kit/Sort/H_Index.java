b5c0d862171c42b4686d8b1bbdf30bda873963c3
victoriaum <y.ilpyo95@gmail.com>
2023-01-04 17:45:32 +0900

    H_Index.java create

---
+++ src/programmers/kit/Sort/H_Index.java
@@ -0,0 +1,17 @@
+package programmers.kit.Sort;
+
+import java.util.*;
+
+// 정렬 - H-Index
+public class H_Index {
+    public static void main(String[] args) {
+        // 어떤 과학자가 발표한 논문의 인용 횟수를 담은 배열 citations가 매개변수로 주어질 때, 이 과학자의 H-Index를 return 하도록 solution 함수를 작성해주세요.
+
+        System.out.println(solution(new int[]{3, 0, 6, 1, 5}));            // 3
+    }
+
+    private static int solution(int[] citations) {
+
+        return 0;
+    }
+}
