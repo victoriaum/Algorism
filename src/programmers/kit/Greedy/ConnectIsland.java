395a5d14b971b624e95abd286915526be0871362
victoriaum <y.ilpyo95@gmail.com>
2023-01-10 11:24:53 +0900

    ConnectIsland.java create

---
+++ src/programmers/kit/Greedy/ConnectIsland.java
@@ -0,0 +1,24 @@
+package programmers.kit.Greedy;
+
+import java.util.*;
+
+// 그리디 - 섬 연결하기
+public class ConnectIsland {
+    public static void main(String[] args) {
+        // n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
+        // 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다. 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
+        // 같은 연결은 두 번 주어지지 않습니다. 또한 순서가 바뀌더라도 같은 연결로 봅니다. 즉 0과 1 사이를 연결하는 비용이 주어졌을 때, 1과 0의 비용이 주어지지 않습니다.
+
+        int[][] costs = new int[][]{{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+        System.out.println(solution(4,  costs));         // 4
+    }
+
+    private static int solution(int n, int[][] costs) {
+        int answer = 0, cnt = 0;
+        int[] passed = new int[n];
+        for(int[] i: costs){
+
+        }
+        return 0;
+    }
+}
