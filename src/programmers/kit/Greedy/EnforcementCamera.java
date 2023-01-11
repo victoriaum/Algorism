1f0a2b1e1b8981111ffda80f4c7f2010e40a88d8
victoriaum <y.ilpyo95@gmail.com>
2023-01-11 10:34:34 +0900

    EnforcementCamera.java update

--- src/programmers/kit/Greedy/EnforcementCamera.java
+++ src/programmers/kit/Greedy/EnforcementCamera.java
@@ -14,13 +14,13 @@ public class EnforcementCamera {
         //int[][] routes = new int[][]{{-2,-1}, {1,2}, {-3,0}};  // 2
 
 
-        /*print(solution([[-2,-1], [1,2],[-3,0]])) #2
-        print(solution([[0,0],])) #1
+        /*
         print(solution([[0,1], [0,1], [1,2]])) #1
         print(solution([[0,1], [2,3], [4,5], [6,7]])) #4
         print(solution([[-20,-15], [-14,-5], [-18,-13], [-5,-3]])) #2
         print(solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]])) #2
-        print(solution([[-20,15], [-20,-15], [-14,-5], [-18,-13], [-5,-3]])) #2*/
+        print(solution([[-20,15], [-20,-15], [-14,-5], [-18,-13], [-5,-3]])) #2
+        */
 
         System.out.println(solution(routes));
     }
